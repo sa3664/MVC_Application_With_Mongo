93f757d7a9a80b6aebe0f9f57140fff4375da4a6
Shivani Aggarwal <shivani.aggarwal2422@gmail.com>
2018-07-04 21:15:46 +0530

    Initial_Commit
    The data is getting saved in the mongo db from the view .

--- MVC_Application_With_Mongo/Controllers/UserController.cs
+++ MVC_Application_With_Mongo/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using MongoDB.Driver.Linq;
 using MVC_Application_With_Mongo.Models;
 
@@ -32,12 +33,10 @@ namespace MVC_Application_With_Mongo.Controllers
             MongoClient client = new MongoClient(connectionString);
             MongoServer objServer = client.GetServer();
 
-
-            //ConfigurationManager.AppSettings["
             //MongoServer objServer = MongoServer.Create("Server=localhost:27017");
             //Provide a database name(Mongo server will automatically check a
             //database with this name while inserting.
-            //if exist then ok otherwise it will create automatically create a database)
+            //if exist then ok otherwise it will  automatically create a database)
             MongoDatabase objDatabse = objServer.GetDatabase("MVCTestDB");
             //Provide a table Name(Mongo will create a table with this name.
             //if its already exist then mongo will insert in this table otherwise
@@ -47,5 +46,37 @@ namespace MVC_Application_With_Mongo.Controllers
             UserDetails.Insert<UserModel>(um);
             return View();
         }
+        //[HttpGet]
+        //public ActionResult GetUsers()
+        //{
+        //    var connectionString = "mongodb://localhost";
+        //    MongoClient client = new MongoClient(connectionString);
+        //    MongoServer objServer = client.GetServer();
+        //    MongoDatabase objDatabse = objServer.GetDatabase("MVCTestDB");
+        //    var UserDetails = objDatabse.GetCollection("Users").FindAll().ToList();
+        //    return View(UserDetails);
+        }
+        //[HttpDelete]
+        //public ActionResult Delete(int id)
+        //{
+        //    var connectionString = "mongodb://localhost";
+        //    MongoClient client = new MongoClient(connectionString);
+        //    MongoServer objServer = client.GetServer();
+        //    MongoDatabase objDatabse = objServer.GetDatabase("MVCTestDB");
+        //    IMongoQuery query = Query.EQ("ID", id);
+        //    objDatabse.GetCollection("Users").Remove(query);
+        //    return View();
+        //}
+        //[HttpPut]
+        //public ActionResult Edit(int id)
+        //{
+        //    var connectionString = "mongodb://localhost";
+        //    MongoClient client = new MongoClient(connectionString);
+        //    MongoServer objServer = client.GetServer();
+        //    MongoDatabase objDatabse = objServer.GetDatabase("MVCTestDB");
+        //    IMongoQuery query = Query.EQ("ID", id);
+        //    UserModel user = objDatabse.GetCollection("Users").Find(query).SingleOrDefault();
+        //    return View(user);
+        //}
     }
 }
